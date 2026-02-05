49e3ed1d9ff51248b63998d14355fc374de52b19
Herwig Cuppens <herwig.cuppens@cegeka.com>
2026-02-05 11:13:04 +0000

    django rest api

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -26,8 +26,12 @@ SECRET_KEY = 'django-insecure-$*hcs381@kh)yk!#sz@mwkfl=!2$-=j&_!)+gv-ou9!ir9$#80
 DEBUG = True
 
 
-# Allow all hosts
-ALLOWED_HOSTS = ['*']
+# Allow localhost and Codespace URL
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
 
 
 # Application definition
